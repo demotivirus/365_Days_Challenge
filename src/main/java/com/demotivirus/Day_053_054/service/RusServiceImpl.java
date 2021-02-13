db36c28f2d7e4b9a87449265187348787361008f
demotivirus <demotivirus@gmail.com>
2021-02-13 23:56:29 +0300

    Day 54: Dictionary Working rus: add word, add translation, add individual translation

--- src/main/java/com/demotivirus/Day_053_054/service/RusServiceImpl.java
+++ src/main/java/com/demotivirus/Day_053_054/service/RusServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +25,10 @@ public class RusServiceImpl implements RusService {
 
     @Override
     public Rus updateWord(Rus rus) {
-        return rusDao.save(rus);
+        rus.setWord(rus.getWord().toLowerCase());
+        Rus parseRus = getRusById(rus.getId());
+        parseRus.setWord(rus.getWord());
+        return rusDao.save(parseRus);
     }
 
     @Override
@@ -36,7 +38,8 @@ public class RusServiceImpl implements RusService {
 
     @Override
     public Rus getRusById(Long id) {
-       var rus = rusDao.getOne(id);
+       //var rus = rusDao.getOne(id);
+       Rus rus = rusDao.findFirstById(id);
        if (rus != null)
            return rusDao.getOne(id);
        else throw new NotFoundException("Not found with id " + id);
@@ -68,29 +71,24 @@ public class RusServiceImpl implements RusService {
 
                 findingRus.addRusEng(rus.getTranslationWord());
 
-                System.err.println("id " + findingRus.getId() + " translation " + findingRus.getTranslationWord());
-                System.err.println(Arrays.asList(findingRus.getRusEng()));
+                System.err.println("Add to word [" + rus.getWord() + "] translation [" + rus.getTranslationWord() + "]");
                 rusDao.save(findingRus);
             }
+            else System.err.println("Not add " + rus.getTranslationWord() + " because translation exists");
         }
     }
 
-//    @Override
-//    public Rus findByWord(String word) {
-//        return rusDao.findByWord(word);
-//    }
-
     @Override
     public Long findIdByWord(String word) {
-        System.err.println("Find id by " + word + " is: " + rusDao.findByWord(word).getId());
+        //System.err.println("Find id by " + word + " is: " + rusDao.findByWord(word).getId());
         return rusDao.findIdByWord(word);
     }
 
     @Override
     public List<String> findAllEngWordsByRusId(Long id) {
         List<String> allEngWordsByRusId = rusDao.findAllEngWordsByRusId(id);
-        for (String s : allEngWordsByRusId)
-            System.err.println(s);
+        //for (String s : allEngWordsByRusId)
+            //System.err.println(s);
         return allEngWordsByRusId;
     }
 
