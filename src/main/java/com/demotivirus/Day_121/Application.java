cb61f33da32eabe616226b62bbd9dbbe1a4e8885
demotivirus <demotivirus@gmail.com>
2021-04-24 00:12:20 +0300

    Day 121: Tests with Mockito. Not working et

---
+++ src/main/java/com/demotivirus/Day_121/Application.java
@@ -0,0 +1,11 @@
+package com.demotivirus.Day_121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
--- src/test/java/com/demotivirus/Day_121/ToDoControllerTest.java
+++ src/test/java/com/demotivirus/Day_121/ToDoControllerTest.java
@@ -1,10 +1,42 @@
 package com.demotivirus.Day_121;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.ws.rs.core.Application;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest
 @ExtendWith(SpringExtension.class)
 public class ToDoControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private ToDoService toDoService;
+
+    @Test
+    public void getAllToDos() throws Exception {
+        List<ToDo> toDos = new ArrayList<>();
+        toDos.add(new ToDo(1L, "New code today", true));
+        toDos.add(new ToDo(2L, "Go to the Walmart", false));
+        when(toDoService.findAll()).thenReturn(toDos);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/todos")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(jsonPath("$", hasSize(2))).andDo(print());
+    }
 }
