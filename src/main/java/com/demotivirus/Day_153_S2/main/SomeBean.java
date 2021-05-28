6be1bd660b709c8c97ecf8d97fb324019b2dd232
demotivirus <demotivirus@gmail.com>
2021-05-28 23:16:50 +0300

    Day 153: Read properties from prop file

---
+++ src/main/java/com/demotivirus/Day_153_S2/main/SomeBean.java
@@ -0,0 +1,12 @@
+package com.demotivirus.Day_153_S2.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SomeBean {
+    private int someValue;
+}
