13078889cd6e2a96eb7716e8d519da6ec37a3f79
demotivirus <demotivirus@gmail.com>
2021-05-28 23:35:25 +0300

    Day 153: Read values from prop file

--- src/main/java/com/demotivirus/Day_153_S2/main/ValuesApp.java
+++ src/main/java/com/demotivirus/Day_153_S2/main/ValuesApp.java
@@ -1,5 +1,7 @@
 package com.demotivirus.Day_153_S2.main;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
