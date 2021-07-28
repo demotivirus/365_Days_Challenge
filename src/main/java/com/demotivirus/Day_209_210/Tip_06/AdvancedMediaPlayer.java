a6af6f467d1a34f8413efee3eb05a2feff185769
demotivirus <demotivirus@gmail.com>
2021-07-29 00:20:55 +0300

    Day 210: Add tips

--- src/main/java/com/demotivirus/Day_209_210/Tip_06/AdvancedMediaPlayer.java
+++ src/main/java/com/demotivirus/Day_209_210/Tip_06/AdvancedMediaPlayer.java
@@ -1,6 +1,5 @@
 package com.demotivirus.Day_209_210.Tip_06;
 
 public interface AdvancedMediaPlayer {
-    void playVlc(String fileName);
-    void playMp3(String fileName);
+    void playMusic(String fileName);
 }
