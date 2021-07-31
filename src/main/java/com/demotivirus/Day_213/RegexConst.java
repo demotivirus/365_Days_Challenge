32ec9a3078b2713cc49385625e468134da7bfa4b
demotivirus <demotivirus@gmail.com>
2021-07-31 20:07:15 +0300

    Fix bug

--- src/main/java/com/demotivirus/Day_213/RegexConst.java
+++ src/main/java/com/demotivirus/Day_213/RegexConst.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum RegexConst {
     SYMBOL_EXCLAMATION("(![^‘’])"),
-    SYMBOL_DOT("\"\\\\b[.]\"");
+    SYMBOL_DOT("\\b[.]");
 
     private String regex;
 }
