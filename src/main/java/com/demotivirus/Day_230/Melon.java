5b19e4f37cf2ddf3a5534ab26fbe66dc29d9bf41
demotivirus <demotivirus@gmail.com>
2021-08-18 20:18:10 +0300

    Day 230: Add java examples

---
+++ src/main/java/com/demotivirus/Day_230/Melon.java
@@ -0,0 +1,11 @@
+package com.demotivirus.Day_230;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Melon {
+    private Type type;
+    private int weight;
+}
